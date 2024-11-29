b320f277d2c66e2cb673fd7bd6f8c71e5e2f5b45
Sabbir Tanvir <sabbirmahmudtanvir@gmail.com>
2024-11-30 00:57:11 +0600

    button

--- my-app/components/Signin.tsx
+++ my-app/components/Signin.tsx
@@ -1,8 +1,7 @@
+import { Button } from "./button";
 
 export const SiginComponent = () => {
-    function handler() {
-        console.log("hello there");
-    }
+
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
@@ -15,7 +14,7 @@ export const SiginComponent = () => {
                     <div className="pt-2">
                         <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                        <button onClick={handler} type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                        <Button />
                     </div>
                 </div>
             </a>
